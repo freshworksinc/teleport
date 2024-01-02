9f41e8f52c5210fd78b6d5d210ef15d10de7749c
Paul Gottschling <paul.gottschling@goteleport.com>
2024-01-02 14:45:55 -0500

    Add inputs for child modules
    Not sure which ones I'll end up needing, but this is good for now!

---
+++ examples/agent-pool-terraform/aws/inputs.tf
@@ -0,0 +1,43 @@
+variable "agent_count" {
+  type        = number
+  description = "Number of agents to deploy"
+}
+
+variable "cloud" {
+  type        = string
+  description = "Cloud provider: aws|gcp|azure"
+  validation {
+    condition     = var.cloud == "aws" || var.cloud == "gcp" || var.cloud == "azure"
+    error_message = "The value of \"cloud\" must be \"aws\", \"gcp\", or \"azure\"."
+  }
+}
+
+variable "proxy_service_address" {
+  type        = string
+  description = "Host and HTTPS port of the Teleport Proxy Service"
+}
+
+variable "region" {
+  type        = string
+  description = "Location in which to deploy agents (Azure location, AWS or GCP region)"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Cloud provider subnet for deploying Teleport agents (subnet ID if using AWS or Azure, name or self link if using GCP)"
+}
+
+variable "teleport_edition" {
+  type        = string
+  default     = "oss"
+  description = "Edition of your Teleport cluster. Can be: oss, enterprise, team, or cloud."
+  validation {
+    condition     = contains(["oss", "enterprise", "team", "cloud"], var.teleport_edition)
+    error_message = "teleport_edition must be one of: oss, enterprise, team, cloud."
+  }
+}
+
+variable "teleport_version" {
+  type        = string
+  description = "Version of Teleport to install on each agent"
+}
---
+++ examples/agent-pool-terraform/teleport/inputs.tf
@@ -0,0 +1,20 @@
+variable "proxy_service_address" {
+  type        = string
+  description = "Host and HTTPS port of the Teleport Proxy Service"
+}
+
+variable "teleport_edition" {
+  type        = string
+  default     = "oss"
+  description = "Edition of your Teleport cluster. Can be: oss, enterprise, team, or cloud."
+  validation {
+    condition     = contains(["oss", "enterprise", "team", "cloud"], var.teleport_edition)
+    error_message = "teleport_edition must be one of: oss, enterprise, team, cloud."
+  }
+}
+
+variable "teleport_version" {
+  type        = string
+  description = "Version of Teleport to install on each agent"
+}
+
