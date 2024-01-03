3da7b9eb86aa679e6fb79ae5c73e5283b21ac47b
Paul Gottschling <paul.gottschling@goteleport.com>
2024-01-03 11:43:37 -0500

    Expose the agent config to the root module

--- examples/agent-pool-terraform/teleport/outputs.tf
+++ examples/agent-pool-terraform/teleport/outputs.tf
@@ -1,13 +1,15 @@
 locals {
   userdata = templatefile("./userdata", {
-    token                 = teleport_provision_token.agent[count.index].metadata.name
-    proxy_service_address = var.proxy_service_address
-    teleport_edition      = var.teleport_edition
-    teleport_version      = var.teleport_version
-  }
+    agent_config = templatefile("${path.root}/agent-config.yaml", {
+      proxy_service_address = var.proxy_service_address
+    })
+    teleport_edition = var.teleport_edition
+    teleport_version = var.teleport_version
+    token            = teleport_provision_token.agent[count.index].metadata.name
+  })
 }
 
 output "userdata" {
-  value = local.userdata
+  value       = local.userdata
   description = "User data script to run on agent instances."
 }
